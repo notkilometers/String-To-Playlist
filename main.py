76b0653c4ec419690758a10907e5532c58932e2b
notkilometers <47023811+notkilometers@users.noreply.github.com>
2021-07-20 13:27:45 -0700

    changed search to be more eff.
    linked list was totally unnecessary, as was the method of joining strings, also fixing and removing any string strippings that were needed as a result of inefficient joinings

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, render_template
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 from itertools import combinations
+import spotipy
 
-client_id = ''
-client_secret = ''
+client_id = '' # put spotify client id here
+client_secret = '' # put spotify client secret here
 
 def login(username):
     token = spotipy.util.prompt_for_user_token(username,scope="playlist-modify-public user-library-modify",client_id=client_id,client_secret=client_secret,redirect_uri="http://localhost:8080/callback") # login w/ playlist and library perms
@@ -22,53 +21,35 @@ def createSpotifyPlaylist(username, name, sp): # makes playlist with passed name
     return playlist["id"]
 
 def addSongsToPlaylist(username, playID, songIDs, sp): # adds songs in list to playlist
-    sp.user_playlist_add_tracks(username, playlist_id=playID, tracks=songIDs)
-  
-def createLList(fullstr): # creates linked list of words in string
-    words = fullstr.split(' ') # split string into individual words
-    head = Link(words[0]) # head = first word
-    ptr = head
-    
-    for i in range(1,len(words)): # assemble linked list from word array
-        ptr.next = Link(words[i])
-        ptr = ptr.next
+    sp.user_playlist_add_tracks(username, playlist_id=playID, tracks=songIDs)   
 
-    return head     
+def assemblePossibleSongsDict(fullstr,sp): # assembles dictionary of valid songs
+    possible = {} 
+    words = fullstr.split(' ') # splits string into array of words
+    length = len(words)
 
-def assemblePossibleSongsDict(head,sp): # assembles dictionary of valid songs from substrings present on linked list
-    possible = {}
-    ptr = head
-    while (ptr != None): # loops through for every word 
+    for i in range(0,length): # loops through for every word 
         str = ""
-        ptr2 = ptr
-        while (ptr2 != None): # loops through for every word after current word in first loop
-            str += ptr2.content + " " # appends to combination
-            if (song := searchSongs(str.strip(),sp)): # if valid song name, add to dict w/ song uri 
+        for j in range(i,length): # loops through for every word after and including current word in first loop
+            str += (" " + words[j]) if (str != "") else (words[j]) # appends to string, with leading space only if not empty string
+            if (song := searchSongs(str, sp)): # if valid song name, add to dict w/ song uri
                 possible[song["name"]] = song["uri"]
-            ptr2 = ptr2.next
-        ptr = ptr.next
+                
     return possible # returns dictionary of valid song substrings
 
 def assembleSongCombos(songlist, fullstr): # assembles combinations of valid song substrings until it finds an exact match of full string
     for i in range(1, len(songlist)+1): # make all possible i length combinations
         combs = combinations(songlist,i)
         for comb in combs:
-            str = ""
-            for words in comb: # adds all words in combination
-                str += words + " "
-                if (str.strip() == fullstr): # checks if match
-                    return comb # returns working combination of songs if matches
+            str = " ".join(comb) # join all words in combination to string
+            if (str == fullstr): # checks if match
+                return comb # returns working combination of songs if matches
             
 def assembleListOfIDs(songdict, combo): # assembles into list of uris for playlist addition
     songs = []
     for song in combo:
-        songs.append(songdict[song])
+        songs.append(songdict[song]) # appends uri from dict to list, ordered SA the string
     return songs
-        
-class Link: # link for linked list
-    def __init__(self, content):
-        self.content = content
-        self.next = None
 
 app = Flask(__name__)
 
@@ -81,16 +62,15 @@ def index():
 
         # doing search & assembly
         sp = login(username) # logs into spotify
-        llist = createLList(string) # creates linked list from string
-        poss_songs = assemblePossibleSongsDict(llist,sp) # assembles possible song combinations
-        if (combos := assembleSongCombos(poss_songs, string)): # if valid combo exits, success, form playlist
-            listOfIDs = assembleListOfIDs(poss_songs, combos) # gathers ids of valid combo into list
+        poss_songs = assemblePossibleSongsDict(string,sp) # assembles possible song combinations
+        if (combo := assembleSongCombos(poss_songs, string)): # if valid combo exits, success, form playlist
+            listOfIDs = assembleListOfIDs(poss_songs, combo) # gathers ids of valid combo into list
             pid = createSpotifyPlaylist(username, string, sp) # creates playlist
             addSongsToPlaylist(username, pid, listOfIDs, sp) # adds songs to playlist
             return render_template("form_success.html")
-        else: # valid combo doesn't exist, failure, retry
+        else: # valid combo doesn't exist, failure, retry with new string
             return render_template("form_failure.html")
-    return render_template("form.html")
+    return render_template("form.html") # base form loaded on get req
 
 if __name__=='__main__':
     app.run(host="127.0.0.1", port=8080, debug=True, use_reloader=False)
